5e1006e8562cf127d38b89e2e9249910d05e336f
khalilRhouma <khalil.rhoumaghardedou@ensi-uma.tn>
2018-07-24 09:13:01 +0100

    Link all the window with the welcome scene

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,68 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+    @FXML
+    private JFXButton HowToPlay;
+
+    @FXML
+    private void showDialog(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("HowToPlay.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("How To Play");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        }catch (Exception e){
+            System.out.println("Sorry can't load the window");
+        }
+    }
+    @FXML
+    private void OpenEasy(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Easy4x4.fxml"));
+            Parent Easy = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Takuzu Easy Level");
+            stage.setScene(new Scene(Easy, 265, 358));
+            stage.show();
+        }catch (Exception e){
+            System.out.println("Sorry can't load the window");
+        }
+    }
+    @FXML
+    private void OpenMedium(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Medium6x6.fxml"));
+            Parent Medium = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Takuzu Medium Level");
+            stage.setScene(new Scene(Medium, 395, 494));
+            stage.show();
+        }catch (Exception e){
+            System.out.println("Sorry can't load the window");
+        }
+    }
+    @FXML
+    private void OpenHard(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Hard8x8.fxml"));
+            Parent Hard = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Takuzu Medium Level");
+            stage.setScene(new Scene(Hard, 529, 615));
+            stage.show();
+        }catch (Exception e){
+            System.out.println("Sorry can't load the window");
+        }
+    }
 }
