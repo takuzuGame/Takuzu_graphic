b88e9fab5bf8764177a914fd2fc29157f6b61e21
khalilRhouma <khalil.rhoumaghardedou@ensi-uma.tn>
2018-07-26 22:39:42 +0100

    Takuzu game v1.0

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,7 +1,4 @@
 package sample;
-import com.jfoenix.controls.JFXButton;
-import javafx.fxml.FXML;
-import sample.easyController.*;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
@@ -17,7 +13,6 @@ import javafx.stage.Stage;
 public class Main extends Application  {
 
     Stage window;
-    Button EasyButton;
 
     public static void main(String[] args) {
         launch(args);
@@ -26,23 +21,13 @@ public class Main extends Application  {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
-        //Parent Easy = FXMLLoader.load(getClass().getResource("Easy4x4.fxml"));
-        //Parent Medium = FXMLLoader.load(getClass().getResource("Medium6x6.fxml"));
-        //Parent Hard = FXMLLoader.load(getClass().getResource("Hard8x8.fxml"));
         window = primaryStage;
         Image icon = new Image(getClass().getResourceAsStream("icon.png"));
         window.getIcons().add(icon);
         window.setTitle("Takuzu");
         window.setScene(new Scene(root, 653, 540));
 
-        //window.setScene(new Scene(Easy, 265, 358));
-        //window.setScene(new Scene(Medium, 395, 494));
-        //window.setScene(new Scene(Hard, 529, 615));
 
-       // StackPane layout = new StackPane();
-
-
-        //window.setScene(scene);
         window.show();
         for (int i = 0; i <16 ; i++) {
 
